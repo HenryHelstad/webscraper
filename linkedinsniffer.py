d7d9407a17fc6c3dee344decc943e51f3354b19d
HenryHelstad <Henry.Helstad@gmail.com>
2023-02-11 21:35:48 -0800

    json output

--- linkedinsniffer.py
+++ linkedinsniffer.py
@@ -1,7 +1,9 @@
+import json
 import sys
 import requests
 import re
 from bs4 import BeautifulSoup
+from termcolor import colored
 #searchURL = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Soc%2Banalyst&location=United%20States&position=1&currentJobId=&start="
 
 SEARCHMAX = int(sys.argv[1])
@@ -21,6 +23,17 @@ regex = [(re.compile(r'(comp\stia|comptia)?\s(security\+|sec\+)', re.IGNORECASE)
 
 #takes in an api version of a linked in search url and returns 
 
+listOfJobs = []
+class job:
+    title = ""
+    date = None
+    applicants_number = None
+    score = 0
+    link = ""
+
+class jobEncoder(json.JSONEncoder):
+    def default(self, o):
+        return o.__dict__
 
 def getjoblinks(jobSearchURL): 
     i = 0
@@ -54,20 +67,47 @@ def extractJobInfo(jobList):
     for i in jobList:
         page = requests.get(i)
         soup = BeautifulSoup(page.content, "html.parser")
-        data = soup.find_all("div", {"class", "show-more-less-html__markup"})[0].text
-        title = soup.find_all("h1", {"class", "top-card-layout__title"})[0].text
+        data = soup.find_all("div", {"class", "show-more-less-html__markup"})
+        if data == []:
+            print(colored("\nERROR:::::::::: empty page\n", 'red'), i)
+            continue
+        data = data[0].text
+        title = soup.find_all("h1", {"class", "top-card-layout__title"})
         date = soup.find_all("span", {"class", "posted-time-ago__text topcard__flavor--metadata"})
         applicantNum = soup.find_all("span", {"num-applicants__caption topcard__flavor--metadata topcard__flavor--bullet"})
-        print(applicantNum)
+        a = search(data)
+        if a >= 1:
+            curr = job()
+            curr.score = a
+            curr.link = i
+            if title != []:
+                curr.title = title[0].text.strip()
+            if date != []:
+                curr.date = date[0].text.strip()
+            if applicantNum != []:
+                curr.applicants_number = applicantNum[0].text.strip()
+            listOfJobs.append(curr)
+
+
+        
+        '''print(applicantNum)
         a = search(data)
         if a >= 0:
             print(a, title.strip("\n   "), date[0].text.strip("\n   "), i)
         elif a >= 0:
-            print("****",a, title.strip("\n   "), i)
+            print("****",a, title.strip("\n   "), i)'''
 
-with open(sys.argv[2]) as f:
-    searches = [l.rstrip() for l in f]
+with open(sys.argv[2]) as links:
+    searches = [l.rstrip() for l in links]
 
 print(searches)
 for url in searches:
     extractJobInfo(getjoblinks(url))
+'''
+for a in listOfJobs:
+    print(a.score, a.title, a.date, a.applicants_number, a.link)
+'''
+with open( sys.argv[3], 'w+') as out:
+     for a in listOfJobs:
+         json.dump(a,out, indent=4, cls=jobEncoder)
+
